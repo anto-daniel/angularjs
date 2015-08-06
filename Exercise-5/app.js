709042bd11053aa828c5fa2e919ad49e7c653121
anto-daniel <anto.daniel@gmail.com!DS01is5>
2015-08-07 04:17:18 +0530

    debugged an error. Its working now...

--- Exercise-5/app.js
+++ Exercise-5/app.js
@@ -1,21 +1,21 @@
 (function() {
     var app = angular.module('store', []);
     app.controller('StoreController', function() {
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = [
+    var gems = [
         {
             name: 'Dodecahedron',
             price: 2.95,
             description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
-            canPurchase: true
+            canPurchase: true,
         },
         {
             name: 'Pentagonal gem',
             price: 5.95,
             description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
-            canPurchase: false
+            canPurchase: false,
         }
     ];
 })();
