503cb364b2eec4f93c6b9115d0fc923c2a3bf8e5
anto-daniel <anto.daniel@gmail.com!DS01is5>
2015-08-07 05:20:31 +0530

    images

--- Exercise-6/app.js
+++ Exercise-6/app.js
@@ -9,12 +9,25 @@
             name: 'Dodecahedron',
             price: 2.00,
             description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            images: [
+                {
+                    full: 'dodecahedron-01-full.jpg',
+                    thumb: 'dodecahedron-01-thumb.jpg'
+                },
+            ];
             canPurchase: true,
         },
         {
             name: 'Pentagonal gem',
             price: 5.95,
             description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            images: [
+                {
+                    full: 'pentagonal-01-full.jpg',
+                    thumb: 'pentagonal-01-thumb.jpg'
+                },
+            ];
+
             canPurchase: false,
         }
     ];
