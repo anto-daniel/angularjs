b7440e5fe9ec9f8dee30292959ad1d16e27c6db6
anto-daniel <anto.daniel@gmail.com!DS01is5>
2015-08-07 04:36:23 +0530

    Exercise 6:  using ul and li

---
+++ Exercise-6/app.js
@@ -0,0 +1,21 @@
+(function() {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function() {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            canPurchase: true,
+        },
+        {
+            name: 'Pentagonal gem',
+            price: 5.95,
+            description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            canPurchase: false,
+        }
+    ];
+})();
