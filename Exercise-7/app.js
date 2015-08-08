2a91143f3e81c83a40bf13affa4bdf588cf180d3
anto-daniel <anto.daniel@gmail.com!DS01is5>
2015-08-08 12:27:39 +0530

    tab navigator: ng-click

---
+++ Exercise-7/app.js
@@ -0,0 +1,34 @@
+(function() {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function() {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.00,
+            description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            images: [
+                {
+                    full: 'dodecahedron-01-full.jpg',
+                    thumb: 'dodecahedron-01-thumb.jpg'
+                },
+            ],
+            canPurchase: true,
+        },
+        {
+            name: 'Pentagonal gem',
+            price: 5.95,
+            description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+            images: [
+                {
+                    full: 'pentagonal-01-full.jpg',
+                    thumb: 'pentagonal-01-thumb.jpg'
+                },
+            ],
+
+            canPurchase: false,
+        }
+    ];
+})();
