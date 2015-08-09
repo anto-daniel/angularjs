6c572abd023ccc44e9050a4f5b9dbd23474aabcd
anto-daniel <anto.daniel@gmail.com!DS01is5>
2015-08-09 21:39:08 +0530

    JSLint Corrections

--- Exercise-3/app.js
+++ Exercise-3/app.js
@@ -1,13 +1,16 @@
-(function() {
-    var app = angular.module('store', []);
-    app.controller('StoreController', function() {
+(function () {
+    "use strict";
+    /*global angular*/
+    var app = angular.module('store', []),
+        gem;
+    app.controller('StoreController', function () {
         this.product = gem;
     });
 
-    var gem = {
+    gem = {
         name: 'Dodecahedron',
         price: 2.95,
         description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
         canPurchase: true
-    }
-})();
+    };
+}());
--- Exercise-4/app.js
+++ Exercise-4/app.js
@@ -1,14 +1,17 @@
-(function() {
-    var app = angular.module('store', []);
-    app.controller('StoreController', function() {
+(function () {
+    "use strict";
+    /*global angular*/
+    var app = angular.module('store', []),
+        gems;
+    app.controller('StoreController', function () {
         this.product = gems;
     });
 
-    var gems = {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
-            canPurchase: false,
-            soldOut: false
-        }
-})();
+    gems = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Some gems have hidden qualities beyond their cluster, beyond their shine...Dodeca is one of those gems',
+        canPurchase: false,
+        soldOut: false
+    };
+}());
